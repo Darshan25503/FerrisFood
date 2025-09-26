0d9ec85f7d1da1e72656f7b58fb383af74a3769a
Nibha Bharati <nibha.bharati@rapidops.co>
2025-09-26 14:37:42 +0530

    folder structure

---
+++ services/auth-service/target/owl/debug/deps/auth_service-2f811353dca52396.d
@@ -0,0 +1,5 @@
+/home/ad.rapidops.com/nibha.bharati/Desktop/practice/FerrisFood/services/auth-service/target/owl/debug/deps/libauth_service-2f811353dca52396.rmeta: src/main.rs
+
+/home/ad.rapidops.com/nibha.bharati/Desktop/practice/FerrisFood/services/auth-service/target/owl/debug/deps/auth_service-2f811353dca52396.d: src/main.rs
+
+src/main.rs:
